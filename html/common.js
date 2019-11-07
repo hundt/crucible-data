796e0c75aeee304a3d047b0b0a9c5c3703b822b3
Chris Hundt <chris@charlesbearllc.com>
2019-11-07 11:38:28 -0500

    Add missing HTML and JS files

---
+++ html/common.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function() {
+    document.getElementById('header').innerHTML = '<a href="items.html">Items</a> | <a href="monsters.html">Monsters</a> | <a href="unique-monsters.html">Unique Monsters</a>';
+});
